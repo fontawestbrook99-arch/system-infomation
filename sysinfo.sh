de19270621ead9a0315a43dc9a045c2e3a97f489
russel fonta <fontawestbrook99@gmail.com>
2026-04-09 13:24:33 +0100

    editing system info file

---
+++ sysinfo.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#!/bin/bash
+echo "=============SYSTEM_INFO==========="
+c_user=$(whoami)
+host_n=$(hostname)
+up_time=$(uptime -p)
+date=$(date +%d-%m-%Y)
+time=$(date +%H:%M:%S)
+total_mem=$(free -m | awk '/^Mem:/ {print $2}')
+used_mem=$(free -m | awk '/^Mem:/ {print $3}')
+free_mem=$(free -m | awk '/^Mem:/ {print $4}')
+echo "CURRENT_USER: $c_user |CURRENT_DATE: $date |CURRENT_TIME: $time |HOSTNAME: $host_n"
+echo "-------Uptime------"
+echo "UPTIME: $up_time"
+echo ""
+echo "------------------------"
+#display 
+echo "TOTAL_MEMORY: $total_mem MB |USED_MEMORY: $used_mem MB |FREE_MEMORY: $free_mem MB"
+echo "------------------------"
+# display disk space,used space,and free space
+total_disk=$(df -m / | awk 'NR==2 {print $2}')
+used_disk=$(df -m / | awk 'NR==2 {print $3}')
+free_disk=$(df -m / | awk 'NR==2 {print $4}')
+echo "TOTAL_DISK_SPACE: $total_disk GB |USED_DISK_SPACE: $used_disk GB |FREE_DISK_SPACE: $free_disk GB"
+echo "  "
+echo "-------PROCESSES---RUNNING--------"
+#total number of running processes
+total_pros=$(ps -e | tail -n +2 | wc -l)
+echo "TOTAL PROCESSES RUNNING: $total_pros"
+echo "  "
+echo "---------------TOP 5 CONSUMING PROCESSES------------------"
+echo "    "
+(ps -eo user,pid,pcpu,pmem,comm --sort=-pcpu | head -n 6) <<< "top_consum"
+echo "$top_consum"
